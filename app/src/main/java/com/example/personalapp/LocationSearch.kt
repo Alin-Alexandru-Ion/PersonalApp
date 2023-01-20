1d82d802947e0c5e69c01a336a2972ba76cb4abe
Alin-Alexandru-Ion <ionalinalexandru@gmail.com>
2023-01-20 19:36:31 +0100

    added activity for searching and passing variable to the main activity

---
+++ app/src/main/java/com/example/personalapp/LocationSearch.kt
@@ -0,0 +1,24 @@
+package com.example.personalapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+class LocationSearch : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_location_search)
+
+        val buttonClick = findViewById<Button>(R.id.searchButton)
+        val searchBox = findViewById <EditText> (R.id.searchBox)
+
+        buttonClick.setOnClickListener {
+            val passable = searchBox.text.toString()
+            val intent = Intent (applicationContext, MainActivity::class.java)
+            intent.putExtra("Test", passable)
+            startActivity(intent)
+        }
+    }
+}
--- app/src/main/java/com/example/personalapp/MainActivity.kt
+++ app/src/main/java/com/example/personalapp/MainActivity.kt
@@ -3,65 +3,68 @@ package com.example.personalapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
-import kotlinx.serialization.Serializable
 import okhttp3.*
 import org.json.JSONObject
 import org.json.JSONTokener
 import java.io.IOException
+import com.squareup.picasso.Picasso
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val urlDump = findViewById <TextView> (R.id.urlDump)
-        val urlButton = findViewById <Button> (R.id.urlButton)
+        val intent = getIntent()
+        var intrare = intent.getStringExtra("Test").toString()
 
-        var localitate = "Vedea"
+        val imageView = findViewById<ImageView>(R.id.iconUrl)
+        var temperatura = findViewById<TextView>(R.id.tempDegree)
+        var locatie = findViewById<TextView>(R.id.locationBox)
+        var regiune = findViewById<TextView>(R.id.regionBox)
+        var urlPoza = "Blank"
+        var grade = "0"
 
-        urlButton.setOnClickListener()
-        {
-            val client = OkHttpClient()
-            val request = Request.Builder()
-                .url("https://weatherapi-com.p.rapidapi.com/forecast.json?q=$localitate&days=1")
-                .get()
-                .addHeader("X-RapidAPI-Key", "7a584c54b7msh85a521175a1bcefp15860cjsn74e20d2d80e2")
-                .addHeader("X-RapidAPI-Host", "weatherapi-com.p.rapidapi.com")
-                .build()
+        val client = OkHttpClient()
+        val request = Request.Builder()
+            .url("https://weatherapi-com.p.rapidapi.com/forecast.json?q=$intrare&days=1")
+            .get()
+            .addHeader("X-RapidAPI-Key", "7a584c54b7msh85a521175a1bcefp15860cjsn74e20d2d80e2")
+            .addHeader("X-RapidAPI-Host", "weatherapi-com.p.rapidapi.com")
+            .build()
 
-            client.newCall(request).enqueue(object: Callback{
-                override fun onFailure(call: Call, e: IOException)
-                {
-                    e.printStackTrace()
-                }
-                override fun onResponse(call: Call, response: Response)
-                {
-                    Log.i("Response", "Response received")
-                    response.use {
-                        if(!response.isSuccessful)
-                        {
-                            Log.e("HTTP Error", "Something didn't went well")
-                        }
-                        else
-                        {
-                            val jsonResponse = JSONTokener(response.body?.string()).nextValue() as JSONObject
-                            urlDump.text = jsonResponse.toString()
+        client.newCall(request).enqueue(object: Callback{
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
+            }
+            override fun onResponse(call: Call, response: Response) {
+                response.use {
+                    if(!response.isSuccessful) {
+                        Log.e("HTTP Error", "Something didn't went well")
+                    }
+                    else {
+                        val jsonResponse = JSONTokener(response.body?.string()).nextValue() as JSONObject
 
-                            val loc = jsonResponse.getString("location")
-                            Log.i("ID: ", loc)
+                        val bazaLocation = jsonResponse.getJSONObject("location")
+                        val bazaCurr = jsonResponse.getJSONObject("current")
+                            val bazaCond = bazaCurr.getJSONObject("condition")
 
-                            val curr = jsonResponse.getString("current")
-                            Log.i("ID: ", curr)
-
-                            val bazaGrade = jsonResponse.getJSONObject("current")
-                            val grade = bazaGrade.getString("temp_c")
-                            Log.i("Celsius Degrees: ", grade)
-                        }
+                        locatie.text = bazaLocation.getString("name")
+                        regiune.text = bazaLocation.getString("region") + ", " + bazaLocation.getString("country")
+                        grade = bazaCurr.getString("temp_c")
+                            urlPoza = bazaCond.getString("icon")
                     }
                 }
-            })
-        }
+            }
+        })
+        Thread.sleep(1_000)
+
+        var imageURL = "https:" + urlPoza
+        Picasso.with(this).load(imageURL).into(imageView)
+        temperatura.text = grade + " C"
+
+        Picasso.with(this).load(imageURL).into(imageView)
+        temperatura.text = grade + " C"
     }
 }
